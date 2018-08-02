0008577737f979cc9946120a0cad86033692f484
baro <baok2n@gmail.com>
2018-08-02 23:57:30 +0700

    add bundling and liting to project

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
+
+const port = 3000;
 const app = express();
-var err;
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, ('../src/index.html')));
   // res.send('Hello World!');
 });
 
-app.listen(3000, err, () => {
+app.listen(port, (err) => {
   if (err) {
       console.log(err);
   }
   else {
-      open('http://localhost:3000')
+      open('http://localhost:' + port)
   }
 });
