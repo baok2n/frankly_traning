79e8ea340eedf8ec682e299c63772674bf474849
Administrator <thanhgiangng@gmail.com>
2018-08-02 17:05:03 +0700

    Add automation scripts into package.json

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+const app = express();
+var err;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, ('../src/index.html')));
+  // res.send('Hello World!');
+});
+
+app.listen(3000, err, () => {
+  if (err) {
+      console.log(err);
+  }
+  else {
+      open('http://localhost:3000')
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+
+};
