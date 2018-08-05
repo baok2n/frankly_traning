48d9e9cddd880255a0e10f8af74405f6a28adbc2
baro <baok2n@gmail.com>
2018-08-05 23:18:10 +0700

    add latest code

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,15 @@ app.get('/', (req, res) => {
   // res.send('Hello World!');
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"First name1","lastName": "Last name1", "email": "email1@gamil.com"},
+    {"id": 2,"firstName":"First name2","lastName": "Last name2", "email": "email2@gamil.com"},
+    {"id": 3,"firstName":"First name3","lastName": "Last name3", "email": "email3@gamil.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
       console.log(err);
