a4428c1c9a3a70101ee5f9d77430ed952c4158db
baro <baok2n@gmail.com>
2018-08-06 21:43:32 +0700

    add production build and tweaks for it

---
+++ buildScripts/distServer.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, ('../dist/index.html')));
+  // res.send('Hello World!');
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"First name1","lastName": "Last name1", "email": "email1@gamil.com"},
+    {"id": 2,"firstName":"First name2","lastName": "Last name2", "email": "email2@gamil.com"},
+    {"id": 3,"firstName":"First name3","lastName": "Last name3", "email": "email3@gamil.com"}
+  ]);
+});
+
+app.listen(port, (err) => {
+  if (err) {
+      console.log(err);
+  }
+  else {
+      open('http://localhost:' + port)
+  }
+});
