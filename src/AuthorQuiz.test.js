8b611e1be14393c83baef8b2e70242a9554693e1
baro <baok2n@gmail.com>
2018-08-16 00:15:51 +0700

    handle onClick event when choosing answer in authorquiz

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,11 +1,21 @@
-// import React from 'react'; // eslint-disable-line no-unused-vars
-// import ReactDOM from 'react-dom';
-// import AuthorQuiz from './AuthorQuiz'; // eslint-disable-line no-unused-vars
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz'; // eslint-disable-line no-unused-vars
+// import jsdom from'jsdom';
+import jsdom from 'mocha-jsdom';
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter()});
+
+// before(() => {
+//   jsdom();
+// })
 
 // describe('Author Quiz', () => {
-//     it("render without crashing", () => {
-//         const div = document.createElement("div");
-//         ReactDOM.render(<AuthorQuiz />, div);
-//     })
+  
+//   it("render without crashing", () => {
+//       const div = document.createElement("div");
+//       ReactDOM.render(<AuthorQuiz />, div);
+//   })
 // })
 
