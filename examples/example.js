e59862de307339f2c4022fd16bf670c89c50bd2c
Administrator <thanhgiangng@gmail.com>
2018-08-20 13:25:57 +0700

    add async/await to example

--- examples/example.js
+++ examples/example.js
@@ -95,7 +95,7 @@ console.log("-----------------------------------------------");
 // Promises
 
 function askForCar(luck) {
-    return Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         if (luck) {
             resolve("Yay a car");
         } else {
@@ -114,3 +114,44 @@ truePromise.then(() => {
 }).catch((err) => {
     console.log(err);
 });
+
+console.log("-----------------------------------------------");
+
+var resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve(20);
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+var resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve(10);
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+var concurrentStart = async function () {
+    console.log('==CONCURRENT START with await==');
+    const slow = resolveAfter2Seconds(); // starts timer immediately
+    const fast = resolveAfter1Second();
+
+    console.log(await slow);
+    console.log(await fast); // waits for slow to finish, even though fast is already done!
+}
+
+var stillSerial = function () {
+    console.log('==CONCURRENT START with Promise.all==');
+    Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then(([slow, fast]) => {
+        console.log(slow);
+        console.log(fast);
+    });
+}
+
+concurrentStart();
